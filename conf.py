cab1cce1c53954b184763ba70544298703412eaa
vishdoms <vishdikki@gmail.com>
2025-07-20 08:22:48 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -9,5 +9,5 @@ html_title = ""
 
 # Add any paths that contain custom static files (such as style sheets)
 # here, relative to this directory. These files are copied after the built-in
-# static files, so a file named "default.css" will overwrite the vjdig code built-in one.
+# static files, so a file named "default.css" will overwrite the copychutiye code built-in one.
 # html_static_path = ['_static']
